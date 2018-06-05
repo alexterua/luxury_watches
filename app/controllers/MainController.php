52eea98dd522b72c1920f2cbf8fb4f959a137a1d
alexterua <alexter.od.ua@gmail.com>
2018-06-06 00:52:11 +0300

    Реализован вывод популярных товаров

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -17,7 +17,8 @@ class MainController extends AppController {
 
     public function indexAction() {
         $brands = R::find('brand', 'LIMIT 3');
+        $hits = R::find('product', "hit ='1' AND status = '1' LIMIT 8");
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевые слова...');
-        $this->set(compact('brands'));
+        $this->set(compact('brands', 'hits'));
     }
 }
